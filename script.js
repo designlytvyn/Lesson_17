7565c1ba402d4374dce160a2ca60ab6e7f4affa8
designlytvyn <design.lytvyn@gmail.com>
2023-02-22 16:22:09 +0100

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+console.log(`You can ignore single and double quotes like this: \\'\\' \\" \\"`)
+
+let yourData = alert('Please enter your data')
+console.log(yourData)
+let yourName = prompt('Enter your name', 'Name')
+alert(`Your name is:${yourName}`)
+let yourSurName = prompt('Enter your surname', 'Surname')
+alert(`Your ful name is:${yourName}  ${yourSurName}`)
+let yourAre = prompt('How old are you', 'Age')
+alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}`)
+let youAdmin = confirm('Are you admin?')
+alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}, admin status:${youAdmin}`)
