e36040986e34206c16251511a8713548266b374e
designlytvyn <design.lytvyn@gmail.com>
2023-02-22 22:38:25 +0100

    final

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict'
 console.log(`You can ignore single and double quotes like this: \\'\\' \\" \\"`)
 
-let yourData = alert('Please enter your data')
-console.log(yourData)
-let yourName = prompt('Enter your name', 'Name')
-alert(`Your name is:${yourName}`)
-let yourSurName = prompt('Enter your surname', 'Surname')
-alert(`Your ful name is:${yourName}  ${yourSurName}`)
-let yourAre = prompt('How old are you', 'Age')
-alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}`)
-let youAdmin = confirm('Are you admin?')
-alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}, admin status:${youAdmin}`)
+//let yourData = alert('Please enter your data')
+//console.log(yourData)
+//let yourName = prompt('Enter your name', 'Name')
+//alert(`Your name is:${yourName}`)
+//let yourSurName = prompt('Enter your surname', 'Surname')
+//alert(`Your ful name is:${yourName}  ${yourSurName}`)
+//let yourAre = prompt('How old are you', 'Age')
+//alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}`)
+//let youAdmin = confirm('Are you admin?')
+//alert(`Your ful name is:${yourName}  ${yourSurName}, your age is:${yourAre}, admin status:${youAdmin}`)
+console.log(alert(('5')));
+console.log(true > false);
+console.log(true > 5);
+console.log(true > '5');
+console.log(true > null);
